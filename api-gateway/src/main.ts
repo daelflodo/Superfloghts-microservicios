f68a729ac63d9f46943c93574c3652a9fd515be3
daelflodo <davidflodo@gmail.com>
2024-03-07 22:50:49 -0500

    Estructura y configuracion del api-gateway

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+
+  const options = new DocumentBuilder()
+    .setTitle('SuperFligt API')
+    .setDescription('App de Vuelos Programados')
+    .setVersion('2.0.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('/api/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
+
+  // Generar configuración de Swagger como JSON
+  const swaggerJson = JSON.stringify(document, null, 2);
+
+  // Escribir el JSON en un archivo
+  fs.writeFileSync('swagger.json', swaggerJson);
+
+  await app.listen(PORT, '0.0.0.0', () => {
+    console.log('Listening in port:' + PORT);
+  });
 }
 bootstrap();
---
+++ api-gateway/src/user/user.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { ApiTags } from '@nestjs/swagger';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { ClientProxySuperFlights } from 'src/common/proxy/client-proxy';
+import { IUser } from 'src/common/interfaces/user.interface';
+import { UserMSG } from 'src/common/constanst';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+
+@ApiTags('users')
+@UseGuards(JwtAuthGuard)
+@Controller('/api/v1/user')
+export class UserController {
+  constructor(
+    private readonly clientProxy: ClientProxySuperFlights,
+  ) {}
+  private _clientProxyUser = this.clientProxy.clientProxyUsers();
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Observable<IUser> {
+    return this._clientProxyUser.send(UserMSG.CREATE, createUserDto);
+  }
+
+  @Get()
+  findAll(): Observable<IUser[]> {
+    return this._clientProxyUser.send(UserMSG.FIND_ALL, '');
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Observable<IUser> {
+    return this._clientProxyUser.send(UserMSG.FIND_ONE, id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Observable<IUser> {
+    return this._clientProxyUser.send(UserMSG.UPDATE, { id, updateUserDto });
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this._clientProxyUser.send(UserMSG.DELETE, id);
+  }
+}
